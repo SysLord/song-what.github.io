ebbe7f3fe1c98a0248070370bc99236ed823c471
Xuesong Zhang <xuesong@ahorngmbh.de>
2016-11-13 07:40:54 +0100

    201611130740

--- app_backup.js
+++ app_backup.js
@@ -2,9 +2,10 @@ var app = angular.module('myApp', [], function($interpolateProvider) {
   $interpolateProvider.startSymbol('[[');
   $interpolateProvider.endSymbol(']]');
 });
+
 app.controller('myCtrl', function($scope,$http) {
     $scope.randomCount = 3;
-    $scope.serverIP = "http://192.168.206.24:8080/"; // "http://192.168.204.198:8080/";
+    $scope.serverIP = "http://192.168.206.24:8080/";
 
     $scope.query = "";
 
@@ -25,7 +26,7 @@ app.controller('myCtrl', function($scope,$http) {
     {
       $http.jsonp($scope.serverIP + 'newRezept?callback=JSON_CALLBACK').
           success(function(data, status, headers, config) {
-            //  console.log(JSON.stringify(data) );
+              console.log(JSON.stringify(data) );
               $scope.lastRezeptId = data;
           }).error(function(data, status, headers, config) {
             console.log("error");
@@ -41,35 +42,7 @@ app.controller('myCtrl', function($scope,$http) {
                                                   '&index='+s.indexx+
                                                   '&callback=JSON_CALLBACK').
           success(function(data, status, headers, config) {
-            //  console.log(JSON.stringify(data) );
-          }).error(function(data, status, headers, config) {
-            console.log("error");
-      });
-    }
-
-
-    function changeRezeptName(name)
-    {
-      $http.jsonp($scope.serverIP + '/changeRezeptName?id='+$scope.lastRezeptId+
-                                                  '&name='+name+
-                                                  '&callback=JSON_CALLBACK').
-          success(function(data, status, headers, config) {
-            //  console.log(JSON.stringify(data) );
-          }).error(function(data, status, headers, config) {
-            console.log("error");
-      });
-    }
-
-    function schrittSenden(s)
-    {
-      $http.jsonp($scope.serverIP + 'neuerSchritt?rid='+$scope.lastRezeptId+
-                                                  '&zid='+s.zid+
-                                                  '&beschreibung='+s.desc+
-                                                  '&zeit='+s.zeit+
-                                                  '&index='+s.indexx+
-                                                  '&callback=JSON_CALLBACK').
-          success(function(data, status, headers, config) {
-            //  console.log(JSON.stringify(data) );
+              console.log(JSON.stringify(data) );
           }).error(function(data, status, headers, config) {
             console.log("error");
       });
@@ -80,9 +53,8 @@ app.controller('myCtrl', function($scope,$http) {
       for (var i = 0; i < $scope.schritte.length; i++) {
           schrittSenden($scope.schritte[i]);
       }
-      changeRezeptName($scope.rezeptName);
 
-      $scope.schritte =  [{"zid":'' , "desc":'', "indexx":0,"zeit":5}];
+      schritte =  [{"zid":'' , "desc":'', "indexx":0,"zeit":5}];;
       $scope.lastRezeptId = null;
     }
 
@@ -90,7 +62,7 @@ app.controller('myCtrl', function($scope,$http) {
     {
       $http.jsonp($scope.serverIP + 'rezepteSuche?query='+$scope.query+'&callback=JSON_CALLBACK').
           success(function(data, status, headers, config) {
-            //  console.log(JSON.stringify(data) );
+              console.log(JSON.stringify(data) );
               $scope.rezepte = data;
           }).error(function(data, status, headers, config) {
             console.log("error");
@@ -101,21 +73,9 @@ app.controller('myCtrl', function($scope,$http) {
     {
       $http.jsonp($scope.serverIP + 'getRandomCart?count='+count+'&callback=JSON_CALLBACK').
           success(function(data, status, headers, config) {
-            //  console.log(JSON.stringify(data) );
+              console.log(JSON.stringify(data) );
               $scope.cartScore = data;
               $scope.getCart();
-          }).error(function(data, status, headers, config) {
-            console.log("error");
-      });
-    };
-
-    $scope.getRezept = function (rid) {
-      $http.jsonp($scope.serverIP + 'getRezept?id='+rid+'&callback=JSON_CALLBACK').
-          success(function(data, status, headers, config) {
-            //  console.log(data.found);
-              $scope.rezepte = [data];
-              $scope.rezeptDetails = [data];
-
           }).error(function(data, status, headers, config) {
             console.log("error");
       });
@@ -124,7 +84,7 @@ app.controller('myCtrl', function($scope,$http) {
     $scope.getCart = function () {
       $http.jsonp($scope.serverIP + 'cart?callback=JSON_CALLBACK').
           success(function(data, status, headers, config) {
-            //  console.log(data.found);
+              console.log(data.found);
               $scope.cart = data;
           }).error(function(data, status, headers, config) {
             console.log("error");
@@ -133,45 +93,16 @@ app.controller('myCtrl', function($scope,$http) {
 
     $http.jsonp($scope.serverIP + 'rezepte?callback=JSON_CALLBACK').
         success(function(data, status, headers, config) {
-          //  console.log(data.found);
+            console.log(data.found);
             $scope.rezepte = data;
         }).error(function(data, status, headers, config) {
           console.log("error");
     });
-
-    $scope.findGetParameter =  function  (parameterName) {
-    var result = null,
-        tmp = [];
-    location.search
-    .substr(1)
-        .split("&")
-        .forEach(function (item) {
-        tmp = item.split("=");
-        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-    });
-    return result;
-}
-
-$scope.DetailsView = new function ()
-{
-    var rid = $scope.findGetParameter("rid");
-    if (rid !== undefined)
+    var init = function ()
     {
-      $scope.getRezept(rid);
+      $scope.getCart();
+
+      $scope.randomCart(5);
     }
-}
-
-$scope.RandomCartInit = new function()
-{
-      var random = $scope.findGetParameter("random");
-      if (random !== undefined )
-        $scope.randomCart(random);
-}
-
-    var init = function () {
-      // $scope.randomCart(5);
-       $scope.getCart();
-     }
-    // and fire it after definition
     init();
 });
